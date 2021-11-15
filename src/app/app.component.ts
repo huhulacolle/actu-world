ccacfa55d5e51dbc0c4946fc9bd45c28d6aa9597
Hugo Araujo <araujohugo@msn.com>
2021-11-15 22:07:52 +0100

    changement noms des tabs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+    { title: 'News', url: '/folder/Inbox', icon: 'paper' },
+    { title: 'Favoris', url: '/folder/Favorites', icon: 'heart' },
+    { title: 'Archive', url: '/folder/Archived', icon: 'archive' },
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
 }
