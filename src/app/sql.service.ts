08587f900e84babd85ec4e1200ee53ba9e19d91e
Hugo Araujo <araujohugo@msn.com>
2021-11-29 13:29:34 +0100

    truc que j'ai oublier

--- src/app/sql.service.ts
+++ src/app/sql.service.ts
@@ -1,24 +1,9 @@
 import { Injectable } from '@angular/core';
-import { SQLite, SQLiteObject } from '@ionic-native/sqlite/ngx';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SqlService {
 
-  constructor(private sqlite: SQLite) { }
-
-  create(): void {
-    this.sqlite.create({
-      name: 'data.db',
-      location: 'default'
-    })
-    .then((db: SQLiteObject) => {
-      db.open();
-      db.executeSql('create table test(name VARCHAR(32))', [])
-      .then(() => console.log('Executed SQL'))
-      .catch(e => console.log(e));
-    })
-    .catch(e => console.log(e));
-  }
+  constructor() { }
 }
