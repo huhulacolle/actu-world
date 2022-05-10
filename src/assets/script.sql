43fe1c0395bc2c1dfd06a42d22da9def4270e7a2
Hugo Araujo <66556677+huhulacolle@users.noreply.github.com>
2022-05-10 08:24:51 +0200

    Update script.sql
    (oui il y avais encore un bug)

--- src/assets/script.sql
+++ src/assets/script.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS favoris (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  url varchar(50) NOT NULL,
-  urlToImage varchar(50) NOT NULL,
-  source varchar(50) NOT NULL,
-  title varchar(50) NOT NULL,
-  description varchar(999) NOT NULL,
+  url varchar(50),
+  urlToImage varchar(50),
+  source varchar(50),
+  title varchar(50),
+  description varchar(999),
   content varchar(999)
 )
